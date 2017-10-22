74cd9c9e3f65f45078a4b1e3509a1975c240e25b
funtikar <funtikar@gmail.com>
2017-10-23 01:22:06 +0800

    Add files via upload

--- dictmaker.py
+++ dictmaker.py
@@ -3,3 +3,18 @@ import xlrd
 book = xlrd.open_workbook("kodbandar.xls")
 sh = book.sheet_by_index(0)
 
+d = {}
+
+myList = []
+
+for i in range(0,449):
+	myList.insert(0,sh.cell_value(i,1))
+for i in range(0,449):
+	if myList.count(sh.cell_value(i,1)) != 1:
+		print("jumlah bandar {0} {1}".format(sh.cell_value(i,1),myList.count(sh.cell_value(i,1))))
+
+#for i in range(0,449):
+#	if d[sh.cell_value(i,1)] != None:	
+#		print("{0} already have values".format(sh.cell_value(i,1)))
+#	d[sh.cell_value(i,1)] = str(sh.cell_value(i,0))
+#print(len(d))
